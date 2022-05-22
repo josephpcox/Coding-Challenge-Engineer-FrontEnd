d043d8ffc3aaa49963ca662c5718ea74af8bd3fa
Joseph Cox <joseph.p.coxbb22@gmail.com>
2022-05-22 19:32:47 -0400

    added multi filter, and field select

--- src/components/NetflixTitles.vue
+++ src/components/NetflixTitles.vue
@@ -1,34 +1,38 @@
 <template>
+<div id="app" class="container">
    <div class="overflow-auto">
-     <b-pagination
-      v-model="currentPage"
-      :total-rows="rows"
-      :per-page="perPage"
-      aria-controls="my-table"
-    ></b-pagination>
-    
-    <b-form-input
-      v-model="search"
-      class="input column-filter"
-      type="text" placeholder="Filter By Title"
-    />
-    <b-table hover 
+  <b-field grouped group-multiline>
+    <div v-for="(field, index) in fields"
+      :key="index"
+      class="control">
+      <b-checkbox v-model="field.visible">
+        {{ field.title }}
+      </b-checkbox>
+    </div>
+    <br>
+        <b-select v-model="perPage" :disabled="!isPaginated">
+          <option value="5">5 per page</option>
+            <option value="10">10 per page</option>
+            <option value="15">15 per page</option>
+            <option value="20">20 per page</option>
+          </b-select>
+            
+  </b-field>
+    <b-table  
       id="my-table"
-      :busy="isLoading"
-      :items="filteredTitles"
-      :fields="fields"
+      :data="data"
+      :paginated="isPaginated"
       :per-page="perPage"
-      :current-page="currentPage"
-      small
+      :columns="fields"
+      :sort-icon="sortIcon"
+      size="is-small"
+      aria-next-label="Next page"
+      aria-previous-label="Previous page"
     >
-    <template #table-busy>
-        <div class="text-center text-primary my-2">
-          <b-spinner variant="primary" class="align-middle"></b-spinner>
-          <strong>Loading Titles...</strong>
-        </div>
-      </template>
+  
     </b-table>
   </div>
+</div>
 </template>
 
 <script>
@@ -38,48 +42,125 @@ export default {
   data() {
     return {
       fields: [
-          {
-            key: 'title',
-            label: 'Title',
-            sortable: true
+         {
+            field: 'show_id',
+            title: 'Show ID',
+            label:'Show ID',
+            sortable: true,
+            searchable: true,
+            numeric: false,
+            visible:true,
+            width: '100',
+            centered: true,
           },
           {
-            key: 'type',
+            field: 'type',
+            title: 'Type',
             label: 'Type',
-            sortable: true
+            sortable: true,
+            searchable: true,
+            numeric: false,
+            visible:true,
+            width: '100',
+            centered: true,
           },
           {
-            key: 'director',
+            field: 'title',
+            title: 'Title',
+            label:'Title',
+            sortable: true,
+            searchable: true,
+            numeric: false,
+            visible:true,
+            width: '100',
+            centered: true,
+          },
+          {
+            field: 'director',
+            title: 'Director',
             label: 'Director',
             sortable: true,
+            searchable: true,
+            numeric: false,
+            visible:true,
+            width: '100',
+            centered: true,
           },
           {
-            key: 'rating',
+            field: 'country',
+            title: 'Country',
+            label: 'Country',
+            sortable: true,
+            searchable: true,
+            numeric: false,
+            visible:true,
+            width: '100',
+            centered: true,
+          },
+          {
+            field: 'duration',
+            title: 'Duration',
+            label: 'Duration',
+            sortable: true,
+            searchable: true,
+            numeric: false,
+            visible:true,
+            width: '100',
+            centered: true,
+          },
+          {
+            field: 'listed_in',
+            title: 'Listed In',
+            label: 'Listed In',
+            sortable: true,
+            searchable: true,
+            numeric: false,
+            visible:true,
+            width: '100',
+            centered: true,
+          },
+          {
+            field: 'release_year',
+            title: 'Release Year',
+            label: 'Release Year',
+            sortable: true,
+            searchable: true,
+            numeric: false,
+            visible:true,
+            width: '100',
+            centered: true,
+          },
+          {
+            field: 'rating',
+            title: 'Rating',
             label: 'Rating',
-            sortable: true
+            sortable: true,
+            searchable: true,
+            numeric: false,
+            visible:true,
+            width: '100',
+            centered: true,
           }
         ],
-      items: [],
+      data: [],
       dataKey: 0,
       isPaginated: true,
       paginationPosition: 'both',
       defaultSortDirection: 'asc',
       currentPage: 1,
-      perPage: 50,
+      perPage: 20,
       search: '',
       classNumber: '',
       isLoading: true,
       isFullPage: true,
+      sortIcon: 'arrow-up',
+      sortIconSize: 'is-small',
       excelExport: [],
     };
   },
   computed: {
-    filteredTitles() {
-      return this.items.filter(item => item.title
-        .toLowerCase().includes(this.search.toLowerCase()));
-    },
     rows() {
-        return this.items.length
+        return this.data.length
     }
   },
   mounted() {
@@ -90,10 +171,10 @@ export default {
       this.isLoading = true;
       const path = 'http://34.73.22.157/api/netflix_titles/get_all';
       const { data } = await axios.get(path);
-      this.items = data;
-      console.log('netflix titles',this.items);
-      console.log('first element', this.items);
+      this.data = data;
+      console.log('netflix titles',this.data);
       this.isLoading = false;
+      console.log('hi');
     },
   },
 };
--- src/main.js
+++ src/main.js
@@ -4,10 +4,14 @@ import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 // Import Bootstrap and BootstrapVue CSS files (order is important)
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
+import 'fontawesome'
 Vue.config.productionTip = false
 
 // Make BootstrapVue available throughout your project
 Vue.use(BootstrapVue)
+Vue.use(Buefy)
 // Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin)
 
